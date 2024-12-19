cafdf80939f090c7c5670ded75972a4a57c5089d
Luba-yampo <lubov.yampolskaya@gmail.com>
2024-12-19 12:19:55 +0100

    Test solution

---
+++ Test 1.kt
@@ -0,0 +1,31 @@
+//fun main() {
+//    val grid = listOf(
+//        listOf("O", "X", "X"),
+//        listOf("O", "X", "O"),
+//        listOf("X", "O", "X")
+//    )
+//
+//    for (row in grid) {
+//        println(row.joinToString(" ")) // Each row is printed using joinToString(" "), which combines the elements of the list into a string with spaces between them.
+//    }
+//}
+
+// #1 You are given a number 'n'. Write a Kotlin program to print the square of this number 'n'. Your function should scan a number 'n' as input and as output it should print the square of 'n'.
+
+
+// Write a program that prints the following shape:
+//
+//#
+//##
+//###
+//####
+//#####
+//######
+
+fun main() {
+
+    println("Line print here ")
+    print(42)
+    println("Pay attention to ")
+    print("syntax")
+}
